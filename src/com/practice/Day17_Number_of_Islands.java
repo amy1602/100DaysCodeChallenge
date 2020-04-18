fae85170880432ed9d16728616cd6fdc32280310
nhatt2 <nhatt2@vng.com.vn>
2020-04-18 21:06:24 +0700

    Day17 update.

--- src/com/practice/Day17_Number_of_Islands.java
+++ src/com/practice/Day17_Number_of_Islands.java
@@ -1,6 +1,22 @@
 package com.practice;
 
 public class Day17_Number_of_Islands {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0) return 0;
+        int [][] gridBelongToIsland = new int[grid.length][grid[0].length];
+        int islandCount = 0;
+        for (int i = 0 ; i < grid.length; i++){
+            for (int j = 0 ; j < grid[i].length; j++) {
+                if (grid[i][j] == '1' && gridBelongToIsland[i][j] == 0) {
+                    islandCount ++;
+                    gridBelongToIsland[i][j] = islandCount;
+                    travelGrid(grid, gridBelongToIsland, i, j, grid.length, grid[0].length);
+                }
+            }
+        }
+        return islandCount;
+    }
+    
     public static void travelGrid(char[][] grid, int [][]gridBelongToIsland, int i, int j, int nR, int nC) {
         if (j > 0 && grid[i][j - 1] == '1' && gridBelongToIsland[i][j-1] == 0) {
             gridBelongToIsland[i][j-1] = gridBelongToIsland[i][j];
