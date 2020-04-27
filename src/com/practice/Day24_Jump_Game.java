24bb14e6985c0526e2bb052e2e3e12b639d33441
nhatt2 <nhatt2@vng.com.vn>
2020-04-27 21:53:01 +0700

    Day24

---
+++ src/com/practice/Day24_Jump_Game.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class Day24_Jump_Game {
+    public static boolean canJump(int[] nums) {
+        if (nums.length == 1) return true;
+        int destination = nums.length - 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] >= destination - i) {
+                destination = i;
+            }
+        }
+        return destination == 0;
+    }
+}
