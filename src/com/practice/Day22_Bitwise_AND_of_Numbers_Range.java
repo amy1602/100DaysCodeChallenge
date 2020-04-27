0b21338881ce5746d4f0ef9b06b199178cca59fe
nhatt2 <nhatt2@vng.com.vn>
2020-04-27 21:53:14 +0700

    Day22

--- src/com/practice/Day22_Bitwise_AND_of_Numbers_Range.java
+++ src/com/practice/Day22_Bitwise_AND_of_Numbers_Range.java
@@ -1,20 +1,14 @@
 package com.practice;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class Day22_Bitwise_AND_of_Numbers_Range {
+    LinkedHashMap<Integer, Integer> list = new LinkedHashMap<>();
     public static int rangeBitwiseAnd(int m, int n) {
         while (n > m) {
             n = n & n - 1;
         }
         return m & n;
-    }
-
-    public static int otherSolution(int m, int n) {
-        int shift = 0;
-        while(m < n){ // when equals 0, stop
-            m >>= 1;
-            n >>= 1;
-            shift++;
-        }
-        return m << shift;
     }
 }
