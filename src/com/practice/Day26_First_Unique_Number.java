3f0c7d02a3e61f7106297abffff5b4011cec4dd0
nhatt2 <nhatt2@vng.com.vn>
2020-04-28 21:11:25 +0700

    Day26

---
+++ src/com/practice/Day26_First_Unique_Number.java
@@ -0,0 +1,37 @@
+package com.practice;
+
+import java.util.LinkedHashMap;
+
+public class Day26_First_Unique_Number {
+    LinkedHashMap<Integer, Boolean> uniqueList = new LinkedHashMap<>();
+    public FirstUnique(int[] nums) {
+        for (int num : nums) {
+            if (!uniqueList.containsKey(num)) {
+                uniqueList.put(num,true);
+            } else if (uniqueList.get(num)){
+                uniqueList.remove(num);
+                uniqueList.put(num,false);
+            }
+        }
+    }
+
+    public int showFirstUnique() {
+        if (uniqueList.size() > 0) {
+            for (int key : uniqueList.keySet()) {
+                if (uniqueList.get(key)) {
+                    return key;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public void add(int value) {
+        if (!uniqueList.containsKey(value)) {
+            uniqueList.put(value,true);
+        } else if (uniqueList.get(value)){
+            uniqueList.remove(value);
+            uniqueList.put(value,false);
+        }
+    }
+}
